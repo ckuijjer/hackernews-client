651bf0279ca4359e97850a5cde271d40e5164c89
Casper Kuijjer <casper@kuijjer.com>
2022-11-16 20:39:32 +0100

    bigger QR

--- qr.mjs
+++ qr.mjs
@@ -7,7 +7,7 @@ const qr = await qrcode.toString(`exp://u.expo.dev/update/${updateId}`, {
 });
 
 const qrWithReplacementsForGitHubActionsOutput = qr
-  .replaceAll('\x1b[40m  \x1b[0m', '\u001b[40;30m\u2003\u2003\u001b[0m') // replace 2 black background spaces with 2 black foreground and background em spaces
-  .replaceAll('\x1b[47m  \x1b[0m', '\u001b[47;37m\u2003\u2003\u001b[0m'); // replace 2 white background spaces with 2 white foreground and background em spaces
+  .replaceAll('\x1b[40m  \x1b[0m', '\u001b[40;30m\u2003\u2003\u2003\u001b[0m') // replace 2 black background spaces with 3 black foreground and background em spaces
+  .replaceAll('\x1b[47m  \x1b[0m', '\u001b[47;37m\u2003\u2003\u2003\u001b[0m'); // replace 2 white background spaces with 3 white foreground and background em spaces
 
 console.log(qrWithReplacementsForGitHubActionsOutput);
