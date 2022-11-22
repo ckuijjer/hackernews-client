ff9005be060b1a54aaf7d8bd8640b8675c5041e9
Casper Kuijjer <casper@kuijjer.com>
2022-11-22 17:14:03 +0100

    fix: SafeAreaPaddingBottom

--- src/SafeAreaPaddingBottom.tsx
+++ src/SafeAreaPaddingBottom.tsx
@@ -1,7 +1,7 @@
 import { View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-export const SafeBottomView = () => {
+export const SafeAreaPaddingBottom = () => {
   const insets = useSafeAreaInsets();
 
   return <View style={{ paddingBottom: insets.bottom }} />;
