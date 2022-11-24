c9c7a59741084d96ec1a62cb681b084516191c31
Casper Kuijjer <casper@kuijjer.com>
2022-11-24 09:16:02 +0100

    feat: JSON snapshot of frontpage and 1st story, and mock-hackernews connector

---
+++ src/connectors/mock-hackernews.ts
@@ -0,0 +1,25 @@
+import frontPageData from '../../data/frontpage.json';
+import storyData from '../../data/story-33726816.json';
+
+import { Story, Comment } from '../types';
+
+export const getFrontPage = async () =>
+  frontPageData.map((story) => mapStory(story));
+
+const mapStory = (story): Story => ({
+  ...story,
+  createdAt: new Date(story.createdAt),
+});
+
+export const getStory = async () => {
+  const story = mapStory(storyData);
+  story.comments = storyData.comments?.map((comment) => mapComment(comment));
+
+  return story;
+};
+
+const mapComment = (comment): Comment => ({
+  ...comment,
+  createdAt: new Date(comment.createdAt),
+  comments: comment.comments?.map((comment) => mapComment(comment)),
+});
