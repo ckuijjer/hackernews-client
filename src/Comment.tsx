90a3d49845660170667b18406634e967e10cc7b3
Casper Kuijjer <casper@kuijjer.com>
2022-12-06 20:20:35 +0100

    feat: first swipeable try

--- src/Comment.tsx
+++ src/Comment.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import {
   Pressable,
   Text,
@@ -6,7 +6,10 @@ import {
   View,
   useWindowDimensions,
   PlatformColor,
+  Animated,
 } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Swipeable } from 'react-native-gesture-handler';
 
 import { RenderHtml } from './RenderHtml';
 import { Comment as CommentType } from './types';
@@ -33,34 +36,57 @@ export const Comment = ({
 }: CommentProps) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const { width } = useWindowDimensions();
+  const swipeRef = useRef<Swipeable>();
 
   const commentTextWidth = width - 2 * PADDING_HORIZONTAL - level * LEVEL_WIDTH;
   const numberOfChildren = getNumberOfChildren(comment);
 
+  const renderRightActions = (progress) => {
+    return (
+      <CollapseAction
+        progress={progress}
+        onAction={() => {
+          swipeRef?.current?.close();
+          setIsCollapsed(true);
+        }}
+      />
+    );
+  };
+
   return (
     <>
       {hidden ? null : (
-        <Pressable onPress={() => setIsCollapsed(!isCollapsed)}>
-          <View style={styles.container}>
-            <CommentLevelIndicator level={level} />
-            <View style={[styles.innerContainer, { width: commentTextWidth }]}>
-              <View style={styles.metadataContainer}>
-                <Text style={styles.metadata}>{comment.user}</Text>
-                {isCollapsed ? (
-                  <Icon name="chatbubble-outline">+{numberOfChildren}</Icon>
-                ) : (
-                  <Icon name="time-outline">{timeAgo(comment.createdAt)}</Icon>
+        <Swipeable
+          renderRightActions={renderRightActions}
+          containerStyle={styles.pressableContainer}
+          ref={swipeRef}
+        >
+          <Pressable onPress={() => setIsCollapsed(!isCollapsed)}>
+            <View style={styles.container}>
+              <CommentLevelIndicator level={level} />
+              <View
+                style={[styles.innerContainer, { width: commentTextWidth }]}
+              >
+                <View style={styles.metadataContainer}>
+                  <Text style={styles.metadata}>{comment.user}</Text>
+                  {isCollapsed ? (
+                    <Icon name="chatbubble-outline">+{numberOfChildren}</Icon>
+                  ) : (
+                    <Icon name="time-outline">
+                      {timeAgo(comment.createdAt)}
+                    </Icon>
+                  )}
+                </View>
+                {!isCollapsed && (
+                  <RenderHtml
+                    source={{ html: comment.text ?? '' }}
+                    contentWidth={commentTextWidth}
+                  />
                 )}
               </View>
-              {!isCollapsed && (
-                <RenderHtml
-                  source={{ html: comment.text ?? '' }}
-                  contentWidth={commentTextWidth}
-                />
-              )}
             </View>
-          </View>
-        </Pressable>
+          </Pressable>
+        </Swipeable>
       )}
       {comment.comments.map((childComment) => (
         <Comment
@@ -74,11 +100,45 @@ export const Comment = ({
   );
 };
 
+const CollapseAction = ({
+  progress,
+  onAction,
+}: {
+  progress: Animated.AnimatedInterpolation<number>;
+  onAction: () => void;
+}) => {
+  const trans = progress.interpolate({
+    inputRange: [0, 1],
+    outputRange: [96, 0],
+  });
+
+  if (progress.__getValue() > 2) {
+    onAction();
+  }
+
+  return (
+    <Animated.View
+      style={[styles.actionsContainer, { transform: [{ translateX: trans }] }]}
+    >
+      <Pressable onPress={onAction} style={styles.action}>
+        <Ionicons
+          name="contract"
+          size={24}
+          color="#fff"
+          style={styles.actionIcon}
+        />
+        <Text style={styles.actionText}>Collapse</Text>
+      </Pressable>
+    </Animated.View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     paddingHorizontal: PADDING_HORIZONTAL,
     flexDirection: 'row',
     width: '100%',
+    backgroundColor: PlatformColor('systemBackground'),
   },
   innerContainer: {
     paddingVertical: 8,
@@ -92,5 +152,26 @@ const styles = StyleSheet.create({
     color: PlatformColor('secondaryLabel'),
     fontSize: 15,
     lineHeight: 20,
+  },
+  pressableContainer: {
+    backgroundColor: PlatformColor('link'),
+  },
+  actionsContainer: {
+    flexDirection: 'row',
+    // backgroundColor: PlatformColor('link'),
+  },
+  action: {
+    padding: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: PlatformColor('link'),
+    width: 96,
+  },
+  actionIcon: {
+    // padding: 8,
+  },
+  actionText: {
+    color: '#fff',
+    fontSize: 15,
   },
 });
