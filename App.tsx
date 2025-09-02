d95b9bea4285c63c68b5f0f9d0ea3a5654cae95e
Casper Kuijjer <casper@kuijjer.com>
2025-09-02 20:11:11 +0200

    upgrade to latest expo 53 and removed sentry

--- App.tsx
+++ App.tsx
@@ -9,7 +9,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import * as Sentry from '@sentry/react-native';
 
 import { FrontPageScreen } from './src/screens/FrontPageScreen';
 import { StoryScreen } from './src/screens/StoryScreen';
@@ -26,11 +25,6 @@ configureLogging();
 const Stack = createStackNavigator<StackParamList>();
 
 const queryClient = new QueryClient();
-
-Sentry.init({
-  dsn: 'https://f1a0d36f911a454983bef785fb775170@o4505205662416896.ingest.sentry.io/4505205673033728',
-  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
-});
 
 const App = () => {
   const colorScheme = useColorScheme();
@@ -68,4 +62,4 @@ const App = () => {
   );
 };
 
-export default Sentry.wrap(App);
+export default App;
